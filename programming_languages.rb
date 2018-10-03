7cb99cff6cd2f41753253ceb3f775fec36ed2121
Omid Vatandoost <omidvatan94@gmail.com>
2018-10-03 17:12:42 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |style_key, language|
+    language.each do |language_key, data_type|
+      data_type.each do |data_type_key, data|
+        if new_hash[language_key]
+          new_hash[language_key][:style] << style_key
+        else
+        new_hash[language_key] = {data_type_key => data, :style => [style_key]}
+      end
+  end
+  end
+  end
+  new_hash
 end
